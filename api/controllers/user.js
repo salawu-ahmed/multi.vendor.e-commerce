4107ab057ee705f9cf9e3132ffb8b80687a0c9a2
asally <a.salawu@outlook.com>
2023-06-27 09:17:37 +0000

    nodemailer - send account activation mail succesful

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const User = require('../model/user')
 const path = require('path');
 const { upload } = require('../multer');
+const fs = require('fs')
+const jwt = require('jsonwebtoken')
 const ErrorHandler = require('../utils/ErrorHandler');
+const sendMail = require('../utils/sendMail');
 
 router.post('/create-users', upload.single('file'), async (req, res, next) => {
     try{
@@ -33,14 +36,29 @@ router.post('/create-users', upload.single('file'), async (req, res, next) => {
             avatar: fileUrl
         } 
     
-        const newUser = await User.create(user)
-        res.status(201).json({
-            success: true,
-            newUser
-        })
+        const activationToken = createActivationToken(user)
+        const activationUrl = `http://localhost:3000/activation/${activationToken}`
+        try{
+            await sendMail({
+                email: user.email,
+                subject: "Activate your account",
+                message: `Hello ${user.name}, click on the link to activate your account: ${activationUrl} `
+            })
+            
+        }catch(err){
+            return next(new ErrorHandler(err.message, 400))
+        }
     }catch(err) {
         console.log(err);
     }
 })
+
+// CREATE ACTIVATION TOKEN 
+const createActivationToken = (user) => {
+    return jwt.sign(user, process.env.JWT_ACTIVATION_SECRET,{
+        expiresIn: "5m"
+    })
+}
+jwt.sign
 
 module.exports = router
